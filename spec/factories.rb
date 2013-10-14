000d1b7054aa10039c9f1021cf5ac2c004d98069
Brandon Pizzacalla <bpizzacalla@gmail.com>
2013-10-14 18:00:34 -0400

    Finish User Signup

---
+++ spec/factories.rb
@@ -0,0 +1,9 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Brandon Pizzacalla"
+    email    "bpizza@gmail.com"
+    password "brandon"
+    password_confirmation "brandon"
+  end
+end
+
